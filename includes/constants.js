990079c9cff22faedc3c723ee3fc0e37a33db291
Guilherme Alcantara <guilhermealc@hotmail.com>
2022-09-08 13:34:41 -0300

    add constants

---
+++ includes/constants.js
@@ -0,0 +1,8 @@
+const source_database = "DEVELOPER_SANDBOX";
+const source_schema = "DEMO_SQUARE_ALT13";
+const target_schema = "SQUARE";
+module.exports = {
+  source_database,
+  source_schema,
+  target_schema
+}
