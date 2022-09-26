22a934960f272f56b477baa850cdca372ea09e35
Guilherme Alcantara <guilhermealc@hotmail.com>
2022-09-26 10:01:07 -0300

    Update H_INVOCATION_ID.js

--- includes/H_INVOCATION_ID.js
+++ includes/H_INVOCATION_ID.js
@@ -1,6 +1,7 @@
  module.exports = (params) => {
   return publish("H_INVOCATION_ID",
  {  type: "table",
+     schema: params.target_schema,
 ...params.defaultConfig
 }).query(ctx => ` SELECT GENERATE_UUID() as invocation_id
 `)
